cf9f5287027c38061e86c0474f2af6ca9f1b3a55
Ruben Fonseca <fonseka@gmail.com>
2014-03-25 14:27:34 +0100

    Allow sourcemaps options.

--- lib/requirejs/rails/config.rb
+++ lib/requirejs/rails/config.rb
@@ -57,6 +57,7 @@ module Requirejs::Rails
         dir
         fileExclusionRegExp
         findNestedDependencies
+        generateSourceMaps
         has
         hasOnSave
         include
@@ -101,7 +102,7 @@ module Requirejs::Rails
                                                     "modules" => [ { 'name' => 'application' } ]
         self[:build_config].merge!(self.user_config).slice!(*self.build_config_whitelist)
         case self.loader
-        when :requirejs 
+        when :requirejs
           # nothing to do
         when :almond
           mods = self[:build_config]['modules']
